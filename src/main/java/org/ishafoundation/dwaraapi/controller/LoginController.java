63e7d65a5ceee610fcde7ee327e41c2877809e7e
Prakash Gurumurthy <prakash.gurumurthy@ishafoundation.org>
2020-03-29 12:20:02 +0530

    login split out and CORS enabled. User registration should be only through server and so CORS disabled for useradmin controller

---
+++ src/main/java/org/ishafoundation/dwaraapi/controller/LoginController.java
@@ -0,0 +1,25 @@
+package org.ishafoundation.dwaraapi.controller;
+
+import org.ishafoundation.dwaraapi.db.dao.master.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class LoginController {
+	
+	@Autowired
+	UserDao userDao;
+	
+	// use it for login... only when the jsessionid cookie is either not set or expired...
+	@PostMapping(produces = "application/json")
+	@RequestMapping({ "/login" })
+	public org.ishafoundation.dwaraapi.db.model.master.User login() {
+		String name = SecurityContextHolder.getContext().getAuthentication().getName();
+		return userDao.findByName(name);
+	}
+}
--- src/main/java/org/ishafoundation/dwaraapi/controller/UserAdminController.java
+++ src/main/java/org/ishafoundation/dwaraapi/controller/UserAdminController.java
@@ -6,10 +6,8 @@ import org.ishafoundation.dwaraapi.db.model.master.Priorityband;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -49,13 +47,5 @@ public class UserAdminController {
 		
 		userDao.save(user);
 		return ResponseEntity.status(HttpStatus.OK).body("Done");
-	}
-	
-	// use it for login... only when 
-	@PostMapping(produces = "application/json")
-	@RequestMapping({ "/login" })
-	public org.ishafoundation.dwaraapi.db.model.master.User login() {
-		String name = SecurityContextHolder.getContext().getAuthentication().getName();
-		return userDao.findByName(name);
 	}
 }
