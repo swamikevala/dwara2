a8af31cc664fab4de60f74eca8176184d372ac0c
consolesameer <sameer.dash@ishafoundation.org>
2020-12-24 15:38:21 +0530

    Artifact response file added. Includes response for delete artifact API and Rename artifact API

---
+++ dwara-service/src/main/java/org/ishafoundation/dwaraapi/api/resp/artifact/ArtifactResponse.java
@@ -0,0 +1,43 @@
+package org.ishafoundation.dwaraapi.api.resp.artifact;
+
+public class ArtifactResponse {
+	
+	private int userRequestId;
+	private String action;
+	private String requestedBy;
+	private String requestedAt;
+	private Artifact artifact;
+	
+	public int getUserRequestId() {
+		return userRequestId;
+	}
+	public void setUserRequestId(int userRequestId) {
+		this.userRequestId = userRequestId;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public String getRequestedBy() {
+		return requestedBy;
+	}
+	public void setRequestedBy(String requestedBy) {
+		this.requestedBy = requestedBy;
+	}
+	public String getRequestedAt() {
+		return requestedAt;
+	}
+	public void setRequestedAt(String requestedAt) {
+		this.requestedAt = requestedAt;
+	}
+
+	public Artifact getArtifact() {
+		return artifact;
+	}
+
+	public void setArtifact(Artifact artifact) {
+		this.artifact = artifact;
+	}
+}
