ce5b167f5d34c1094c24e2e580be29ed374bf9c9
consolesameer <sameer.dash@ishafoundation.org>
2020-12-30 17:43:37 +0530

    Added artifact rename

---
+++ dwara-service/src/main/java/org/ishafoundation/dwaraapi/api/req/artifact/ArtifactRenameFile.java
@@ -0,0 +1,12 @@
+package org.ishafoundation.dwaraapi.api.req.artifact;
+
+public class ArtifactRenameFile {
+	private String newName;
+	
+	public String getNewName() {
+		return newName;
+	}
+	public void setNewName(String newName) {
+		this.newName = newName;
+	}
+}
