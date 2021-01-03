acbe79b95e9c5ed12ba6b95d99687a3d19ad499e
Prakash Gurumurthy <prakash.gurumurthy@ishafoundation.org>
2021-01-04 00:59:19 +0530

    updated sql script again

--- dwara-db/src/data/sql/dwara_update_2_0_3_hotfix3.sql
+++ dwara-db/src/data/sql/dwara_update_2_0_3_hotfix3.sql
@@ -4,9 +4,9 @@ SET foreign_key_checks = 0;
 
 INSERT INTO `destination` (`id`, `path`, `use_buffering`) VALUES ('bru', '/mnt/bru/ffv1', 0);
 
-INSERT INTO artifactclass_task (id, artifactclass_id, storagetask_action_id, processingtask_id, config) VALUES (14, 'video-digi-2020-pub', NULL, 'file-copy', '{\"pathname_regex\": \".*.mkv$\", \"destination_id\" : \"bru\"}');
-INSERT INTO artifactclass_task (id, artifactclass_id, storagetask_action_id, processingtask_id, config) VALUES (15, 'video-digi-2020-priv1', NULL, 'file-copy', '{\"pathname_regex\": \".*.mkv$\", \"destination_id\" : \"bru\"}');
-INSERT INTO artifactclass_task (id, artifactclass_id, storagetask_action_id, processingtask_id, config) VALUES (16, 'video-digi-2020-priv2', NULL, 'file-copy', '{\"pathname_regex\": \".*.mkv$\", \"destination_id\" : \"bru\"}');
+INSERT INTO artifactclass_task (id, artifactclass_id, storagetask_action_id, processingtask_id, config) VALUES (17, 'video-digi-2020-pub', NULL, 'file-copy', '{\"pathname_regex\": \".*.mkv$\", \"destination_id\" : \"bru\"}');
+INSERT INTO artifactclass_task (id, artifactclass_id, storagetask_action_id, processingtask_id, config) VALUES (18, 'video-digi-2020-priv1', NULL, 'file-copy', '{\"pathname_regex\": \".*.mkv$\", \"destination_id\" : \"bru\"}');
+INSERT INTO artifactclass_task (id, artifactclass_id, storagetask_action_id, processingtask_id, config) VALUES (19, 'video-digi-2020-priv2', NULL, 'file-copy', '{\"pathname_regex\": \".*.mkv$\", \"destination_id\" : \"bru\"}');
 
 UPDATE `flowelement` SET `display_order` = 8 WHERE `display_order` = 7 and `flow_id` = 'video-digi-2020-flow';
 UPDATE `flowelement` SET `display_order` = 7 WHERE `display_order` = 6 and `flow_id` = 'video-digi-2020-flow';
