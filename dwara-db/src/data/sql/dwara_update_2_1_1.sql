ca1e2ffe0230b91a9d0aa344be85eb7dbf8b7d10
Swami Kevala <swami.kevala@ishafoundation.org>
2021-01-16 20:15:21 +0000

    dwara_update_2_1_1.sql edited online with Bitbucket

--- dwara-db/src/data/sql/dwara_update_2_1_1.sql
+++ dwara-db/src/data/sql/dwara_update_2_1_1.sql
@@ -1,6 +1,6 @@
 SET foreign_key_checks = 0;
 
-SET @counter := <<last-id-value-in-t_file table>>;
+SET @counter := 15184;
 
 insert ignore into t_file (`id`, `checksum`, `deleted`, `directory`, `pathname`, `size`, `artifact_id`, `file_ref_id`, `pathname_checksum`)
 select @counter:= @counter + 1, g.checksum, g.deleted, g.directory, g.pathname, g.size, g.artifact_id, g.file_ref_id, g.pathname_checksum from
@@ -20,7 +20,7 @@ select @counter:= @counter + 1, g.checksum, g.deleted, g.directory, g.pathname, 
 join file1 g on sub.id = g.id;
 
 insert ignore into t_file_volume (`file_id`, `archive_block`, `deleted`, `encrypted`, `verified_at`, `volume_block`, `volume_id`, `header_blocks`)
-select tf.file_id, fv.archive_block, fv.deleted, fv.encrypted, fv.verified_at, fv.volume_block, fv.volume_id, fv.header_blocks 
+select tf.id, fv.archive_block, fv.deleted, fv.encrypted, fv.verified_at, fv.volume_block, fv.volume_id, fv.header_blocks 
 from t_file tf 
    join file1 f on tf.pathname_checksum = f.pathname_checksum
    join file1_volume fv on f.id = fv.file_id;
