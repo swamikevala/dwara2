69072202d8d74fbbb5f45ef56b4bed14cfe7801c
Prakash Gurumurthy <prakash.gurumurthy@ishafoundation.org>
2021-07-08 18:36:35 +0530

    AutoInitializer as API endpoint

--- dwara-core/src/main/java/org/ishafoundation/dwaraapi/scheduler/ScheduledBlankTapeAutoInitializer.java
+++ dwara-core/src/main/java/org/ishafoundation/dwaraapi/scheduler/ScheduledBlankTapeAutoInitializer.java
@@ -15,10 +15,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
 public class ScheduledBlankTapeAutoInitializer {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ScheduledBlankTapeAutoInitializer.class);
