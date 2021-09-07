75e005593ee8feb430d88522619d09166c5fd374
Prakash Gurumurthy <prakash.gurumurthy@ishafoundation.org>
2021-09-07 17:37:18 +0530

    Tape UI - remove tape fix

--- dwara-core/src/main/java/org/ishafoundation/dwaraapi/utils/VolumeUtil.java
+++ dwara-core/src/main/java/org/ishafoundation/dwaraapi/utils/VolumeUtil.java
@@ -144,7 +144,7 @@ public class VolumeUtil {
 		if(jobInProgressCount > 0) {
 			tapeUsageStatus = TapeUsageStatus.job_in_progress;
 		}
-		else if(isQueuedJobOnGroupVolume(volumeDao.findById(volumeId).get().getGroupRef().getId())){
+		else if(isQueuedJobOnVolume(volumeId) || isQueuedJobOnGroupVolume(volumeDao.findById(volumeId).get().getGroupRef().getId())){ // for restores and writes respectively
 			// any group job queued up
 			tapeUsageStatus = TapeUsageStatus.job_queued;
 		}
@@ -155,9 +155,19 @@ public class VolumeUtil {
 		return tapeUsageStatus;
 	}
 	
+	// for writes
 	public boolean isQueuedJobOnGroupVolume(String volumeId){
 		boolean queuedJob = false;
 		long jobInQueueCount = jobDao.countByStoragetaskActionIdIsNotNullAndGroupVolumeIdAndStatus(volumeId, Status.queued);
+		if(jobInQueueCount > 0)
+			queuedJob = true;
+		return queuedJob; 
+	}
+	
+	// for restores
+	public boolean isQueuedJobOnVolume(String volumeId){
+		boolean queuedJob = false;
+		long jobInQueueCount = jobDao.countByStoragetaskActionIdIsNotNullAndVolumeIdAndStatus(volumeId, Status.queued);
 		if(jobInQueueCount > 0)
 			queuedJob = true;
 		return queuedJob; 
