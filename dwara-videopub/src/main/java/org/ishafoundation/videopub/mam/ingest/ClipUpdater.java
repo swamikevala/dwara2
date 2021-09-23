a8bf3537f51601730173bf818dec485f1cff6436
Prakash Gurumurthy <prakash.gurumurthy@ishafoundation.org>
2021-09-23 16:02:44 +0530

    Fix for catdv responding with data too long

--- dwara-videopub/src/main/java/org/ishafoundation/videopub/mam/ingest/ClipUpdater.java
+++ dwara-videopub/src/main/java/org/ishafoundation/videopub/mam/ingest/ClipUpdater.java
@@ -1,7 +1,12 @@
 package org.ishafoundation.videopub.mam.ingest;
 
+import java.io.File;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.ishafoundation.dwaraapi.utils.HttpClientUtil;
+import org.ishafoundation.dwaraapi.utils.JsonPathUtil;
 import org.ishafoundation.videopub.mam.core.CatDVInteractor;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -17,7 +22,7 @@ public class ClipUpdater extends CatDVInteractor {
 
 	// E.g.,
 	// http://localhost:8080/catdv/api/8/clips/27?jsessionid=A214B210360509D08D05624969D55B3F - PUT
-	public String updateClip(String jsessionId, String insertedClipJsonResp, Integer insertedThumbnailID) throws Exception {
+	public String updateClip(String jsessionId, String insertedClipJsonResp, Integer insertedThumbnailID, String metaDataJson, String lowResFilePath) throws Exception {
 		
 	    JSONObject insertedClipJsonRespObject = new JSONObject(insertedClipJsonResp);
 	    JSONObject insertedClipJsonRespDataObject = insertedClipJsonRespObject.getJSONObject("data");
@@ -29,13 +34,29 @@ public class ClipUpdater extends CatDVInteractor {
 	    
         BasicClientCookie cookie = getCookie(jsessionId);	
     	
-	    JSONObject bodyPayload = insertedClipJsonRespDataObject.put("posterID", insertedThumbnailID);
+	    JSONObject bodyPayloadJsonObj = insertedClipJsonRespDataObject.put("posterID", insertedThumbnailID);
 	    int[] thumbnails = {insertedThumbnailID};
-	    bodyPayload = bodyPayload.put("thumbnailIDs", thumbnails);
+	    bodyPayloadJsonObj = bodyPayloadJsonObj.put("thumbnailIDs", thumbnails);
     	
+		String mediaPath = null;
+		String name = null;
+		String bin = null;
+		try {
+			mediaPath = JsonPathUtil.getValue(metaDataJson, "format.filename");
+			name = FilenameUtils.getName(mediaPath);
+			bin = StringUtils.substringAfterLast(FilenameUtils.getFullPathNoEndSeparator(mediaPath), File.separator);
+		} catch (Exception e) {
+			logger.warn("Unable to get media details. So defaulting it to null.");
+		}
+		
+	    String bodyPayload = bodyPayloadJsonObj.toString();
+	    bodyPayload = JsonPathUtil.setValue(bodyPayload, "name", name);
+	    bodyPayload = JsonPathUtil.setValue(bodyPayload, "media.filePath", lowResFilePath);
+	    bodyPayload = JsonPathUtil.setValue(bodyPayload, "importSource.file", mediaPath);
+	    
     	String response = null;
 		try {
-			response = HttpClientUtil.putItWithCookie(cookie, endpointUrl, null, bodyPayload.toString());
+			response = HttpClientUtil.putItWithCookie(cookie, endpointUrl, null, bodyPayload);
 		} catch (Exception e) {
 			String errorMsg = "Unable to update clip " + endpointUrl + " :: "  + e.getMessage();
 			logger.error(errorMsg, e);
