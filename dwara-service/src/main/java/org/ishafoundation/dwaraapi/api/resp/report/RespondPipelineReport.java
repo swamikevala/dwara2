c6a5d3a5ff66e3022212b772a098558b025c0327
Dong Truong <dong.truong@ishafoundation.org>
2021-10-22 20:36:00 +0530

    haftway for pipeline report

---
+++ dwara-service/src/main/java/org/ishafoundation/dwaraapi/api/resp/report/RespondPipelineReport.java
@@ -0,0 +1,13 @@
+package org.ishafoundation.dwaraapi.api.resp.report;
+
+public class RespondPipelineReport {
+    public String[] ingestedArtifacts;
+    public String[] inprogress;
+    public String[] completed;
+    public String[] copy1WriteFailed;
+    public String[] copy2WriteFailed;
+    public String[] copy3WriteFailed;
+    public String[] proxyGenFailed;
+    public String[] mapUpdateFailed;
+    public String[] inStaged;
+}
--- dwara-service/src/main/java/org/ishafoundation/dwaraapi/resource/ReportController.java
+++ dwara-service/src/main/java/org/ishafoundation/dwaraapi/resource/ReportController.java
@@ -1,17 +1,21 @@
 package org.ishafoundation.dwaraapi.resource;
 
+import java.util.Date;
 import java.util.List;
 
 import org.ishafoundation.dwaraapi.api.req.report.RequestReportSize;
+import org.ishafoundation.dwaraapi.api.resp.report.RespondPipelineReport;
 import org.ishafoundation.dwaraapi.api.resp.report.RespondReportSize;
 import org.ishafoundation.dwaraapi.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
@@ -19,6 +23,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReportController {
     @Autowired
     ReportService reportService;
+
+    @GetMapping(value = "/report/pipeline", produces = "application/json")
+	public ResponseEntity<RespondPipelineReport> getPipelineReport(@RequestParam(required=false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date requestedFrom,  @RequestParam(required=false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date requestedTo){
+        RespondPipelineReport report = reportService.getPipelineReport(requestedFrom.toString(), requestedTo.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(report);
+    }
     
     @PostMapping(value="/report/ingestSize", produces = "application/json")
     public ResponseEntity<List<RespondReportSize>> getReportIngestSize(@RequestBody RequestReportSize requestReportIngestSize){
