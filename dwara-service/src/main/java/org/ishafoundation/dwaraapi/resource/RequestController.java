b9748e186a53864d9628cc971fc358f9653d1364
aumrit <aumrit.sarangi@ishafoundation.org>
2021-10-26 20:52:10 +0530

    controler error

--- dwara-service/src/main/java/org/ishafoundation/dwaraapi/resource/RequestController.java
+++ dwara-service/src/main/java/org/ishafoundation/dwaraapi/resource/RequestController.java
@@ -30,6 +30,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.ishafoundation.dwaraapi.api.resp.request.RestoreResponse;
 
 @CrossOrigin
 @RestController
