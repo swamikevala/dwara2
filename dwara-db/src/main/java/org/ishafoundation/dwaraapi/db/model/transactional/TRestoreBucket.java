665cd9ff344cf5419835cda0a44c15a01108f6ba
aumrit <aumrit.sarangi@ishafoundation.org>
2021-11-02 17:06:00 +0530

    blob added

--- dwara-db/src/main/java/org/ishafoundation/dwaraapi/db/model/transactional/TRestoreBucket.java
+++ dwara-db/src/main/java/org/ishafoundation/dwaraapi/db/model/transactional/TRestoreBucket.java
@@ -24,7 +24,7 @@ public class TRestoreBucket {
     @Column(name="createdAt")
     Date createdAt;
     //@Column(name="details")
-    @Type(type = "jsonb")
+    @Lob
     @Column(name="details", columnDefinition = "json")
 
     List<RestoreBucketFile> details ;
