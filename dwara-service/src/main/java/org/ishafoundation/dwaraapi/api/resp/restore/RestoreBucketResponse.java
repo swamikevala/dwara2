a9ff3f3e533cb17fa4ab67119ba85aebf331c1fc
aumrit <aumrit.sarangi@ishafoundation.org>
2021-11-25 16:02:38 +0530

    added creator at and id1

--- dwara-service/src/main/java/org/ishafoundation/dwaraapi/api/resp/restore/RestoreBucketResponse.java
+++ dwara-service/src/main/java/org/ishafoundation/dwaraapi/api/resp/restore/RestoreBucketResponse.java
@@ -123,6 +123,22 @@ RestoreBucketResponse(){
         this.details = details;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
     public RestoreBucketResponse(TRestoreBucket tRestoreBucket){
     this.id=tRestoreBucket.getId();
     this.approvalDate=tRestoreBucket.getApprovalDate();
