ef3b314f06ed161ef6efbf50f8a36544da0734e2
aumrit <aumrit.sarangi@ishafoundation.org>
2021-12-05 18:45:47 +0530

    clip search and allTags

--- dwara-db/src/main/java/org/ishafoundation/dwaraapi/db/dao/transactional/FileDao.java
+++ dwara-db/src/main/java/org/ishafoundation/dwaraapi/db/dao/transactional/FileDao.java
@@ -6,22 +6,22 @@ import org.ishafoundation.dwaraapi.db.model.transactional.File;
 import org.springframework.data.repository.CrudRepository;
 
 public interface FileDao extends CrudRepository<File,Integer> {
-	
-	File findByPathnameChecksum(byte[] filePathnameChecksum);
-	
-	File findByPathname(String pathname);
-	
-	List<File> findAllByArtifactIdAndDeletedFalse(int artifactId);
-	
-	List<File> findAllByArtifactId(int artifactId);
-	
-	List<File> findAllByFileRefId(int fileId);
-	
-	List<File> findAllByFileRefIdAndPathnameEndsWith(int file1Id , String extension);
 
-	List<File> findAllByArtifactIdAndPathnameEndsWith(int id , String extensions);
-	
+	File findByPathnameChecksum(byte[] filePathnameChecksum);
+
+	File findByPathname(String pathname);
+
+	List<File> findAllByArtifactIdAndDeletedFalse(int artifactId);
+
+	List<File> findAllByArtifactId(int artifactId);
+
+	List<File> findAllByFileRefId(int fileId);
+
+	List<File> findAllByFileRefIdAndPathnameEndsWith(int file1Id, String extension);
+
+	List<File> findAllByArtifactIdAndPathnameEndsWith(int id, String extensions);
+
 	List<File> findByPathnameIn(List<String> pathnameList);
-	
+
 	List<File> findByPathnameContains(String keyword);
 }
