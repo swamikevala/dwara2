2b3dffe158196fa5905d0c7f57a150bf7498a3cc
Prakash Gurumurthy <prakash.gurumurthy@ishafoundation.org>
2021-12-20 20:55:19 +0530

    Import xml gen tool - Permissions set for destination dir

--- dwara-misc/dwara-import/src/main/java/org/isha/dwaraimport/Import.java
+++ dwara-misc/dwara-import/src/main/java/org/isha/dwaraimport/Import.java
@@ -2,6 +2,14 @@ package org.isha.dwaraimport;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFileAttributeView;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 
@@ -27,14 +35,37 @@ public class Import {
 			throw new Exception("Artifactclass mapping file missing - " + artifactToArtifactClassMappingJsonFolderPath);
 		
 		java.io.File destination = new java.io.File(destinationXMLLocation);
-		if(!destination.exists())
+		if(!destination.exists()) {
 			FileUtils.forceMkdir(destination);
+			changePermissions(destination, "rwxrwxrwx");
+		}
 		
 		java.io.File destinationFailed = new java.io.File(destinationXMLLocation + "-failed");
-		if(!destinationFailed.exists())
+		if(!destinationFailed.exists()) {
 			FileUtils.forceMkdir(destinationFailed);
-		
+			changePermissions(destination, "rwxrwxrwx");
+		}
 		dwaraImport.apacheGetXMLData(bruFileLocation, artifactToArtifactClassMappingJsonFolderPath, destinationXMLLocation);
     }
+    
+	private static boolean changePermissions(File file, String permissionsRWXForm) throws Exception {
+		Path path = Paths.get(file.getAbsolutePath());
+
+		PosixFileAttributeView posixView = Files.getFileAttributeView(path, PosixFileAttributeView.class);
+		if (posixView == null) {
+			throw new Exception("POSIX attribute view is not supported. Unable to set permissions to " + file.getAbsolutePath());
+		}
+
+		PosixFileAttributes attribs = posixView.readAttributes();
+		Set<PosixFilePermission> currentPermissions = attribs.permissions();
+		String currentPermissionsIn_rwxForm = PosixFilePermissions.toString(currentPermissions);
+		System.out.println("Existing permissions " + currentPermissionsIn_rwxForm);
+		
+		Set<PosixFilePermission> newPermissions = PosixFilePermissions.fromString(permissionsRWXForm);
+		posixView.setPermissions(newPermissions);
+		System.out.println(PosixFilePermissions.toString(newPermissions) + " permissions set successfully.");
+		
+		return true;
+	}
 
 }
