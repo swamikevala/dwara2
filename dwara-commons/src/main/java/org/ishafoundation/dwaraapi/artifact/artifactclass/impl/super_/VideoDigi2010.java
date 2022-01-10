4f108a205df41378e5a7d6b9122e2299e32868cb
Prakash Gurumurthy <prakash.gurumurthy@ishafoundation.org>
2022-01-10 21:47:19 +0530

    Import Digi 2010 fixes

--- dwara-commons/src/main/java/org/ishafoundation/dwaraapi/artifact/artifactclass/impl/super_/VideoDigi2010.java
+++ dwara-commons/src/main/java/org/ishafoundation/dwaraapi/artifact/artifactclass/impl/super_/VideoDigi2010.java
@@ -5,8 +5,6 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,8 +25,16 @@ public class VideoDigi2010 implements Artifactclass{
 	private static final String DV_CODE_REGEX = "[A-Z]{1,2}[\\d]{1,5}";
 	private static final String DV_CAPTURED_REGEX = "_D[Vv]-[Cc]aptured_";
 	private static final String SHIFTED_FROM_REGEX = "_Shifted-From-";  
-    private String SHIFTED_DV_REGEX = "^(" + DV_CODE_REGEX + ")_Shifted-to-(" + DV_CODE_REGEX + ")";
-    private Pattern SHIFTED_DV_REGEX_PATTERN = Pattern.compile(SHIFTED_DV_REGEX);
+	
+	private static final String NUMBER_DV_CAPTURED_DV_CODE_REGEX = "^(" + NUMBER_REGEX + ")" + DV_CAPTURED_REGEX + "(" + DV_CODE_REGEX + ")";
+	private static final Pattern NUMBER_DV_CAPTURED_DV_CODE_REGEX_PATTERN = Pattern.compile(NUMBER_DV_CAPTURED_DV_CODE_REGEX);
+	
+	// 6719_DV-Captured_P204_Shifted-From-M970_Residents-Meet_IIIS_8-Apr-2009_Tape2_Cam1
+	private static final String NUMBER_DV_CAPTURED_DV_CODE_SHIFTED_FROM_DV_CODE_REGEX = NUMBER_DV_CAPTURED_DV_CODE_REGEX + SHIFTED_FROM_REGEX + "(" + DV_CODE_REGEX + ")";
+	private static final Pattern NUMBER_DV_CAPTURED_DV_CODE_SHIFTED_FROM_DV_CODE_REGEX_PATTERN = Pattern.compile(NUMBER_DV_CAPTURED_DV_CODE_SHIFTED_FROM_DV_CODE_REGEX);
+	
+    private static final String SHIFTED_TO_DV_REGEX = "^(" + DV_CODE_REGEX + ")_Shifted-to-(" + DV_CODE_REGEX + ")";
+    private static final Pattern SHIFTED_TO_DV_REGEX_PATTERN = Pattern.compile(SHIFTED_TO_DV_REGEX);
 
 	private static final Map<String, String> SHIFTED_TO_MAP = new HashMap<String, String>();
 	
@@ -52,24 +58,23 @@ public class VideoDigi2010 implements Artifactclass{
 	@Override
 	public ArtifactAttributes getArtifactAttributes(String proposedName) {
 		ArtifactAttributes artifactAttributes = new ArtifactAttributes();
-		
+		Matcher m1 = NUMBER_DV_CAPTURED_DV_CODE_REGEX_PATTERN.matcher(proposedName);
 		if(proposedName.matches("^" + DV_CODE_REGEX + "$")) {
 			if(SHIFTED_TO_MAP.containsKey(proposedName))
 				artifactAttributes.setPreviousCode(getPreviousCodeFromShiftedToName(SHIFTED_TO_MAP.get(proposedName)));
+			else
+				artifactAttributes.setPreviousCode(proposedName);
 			
-			artifactAttributes.setPreviousCode(proposedName);
+		} else if(m1.find()) {  //5902_DV-Captured_A1929_Inner-Engineering_Tampa_Day3_Cam2_Tape2_10-Nov-06
+			int oldSeq = Integer.parseInt(m1.group(1)); //5902
+			artifactAttributes.setPreviousCode(m1.group(1) + "_" + m1.group(2));  //5902_A1929
+			//artifactAttributes.setPreviousCode(m1.group(2) + "_" + m1.group(1));  //A1929_5902
 			
-		} else if(proposedName.matches("^" + NUMBER_REGEX + DV_CAPTURED_REGEX  + DV_CODE_REGEX)) {  //5902_DV-Captured_A1929_Inner-Engineering_Tampa_Day3_Cam2_Tape2_10-Nov-06
-			int idx = ordinalIndexOf(proposedName, "_", 3);
-			String prefix = proposedName.substring(0, idx); //5902_DV-Captured_A1929
-			int oldSeq = Integer.parseInt(prefix.substring(0, prefix.indexOf("_"))); //5902
-			artifactAttributes.setPreviousCode(prefix.replace(DV_CAPTURED_REGEX, ""));  //5902_A1929
-			
+			Matcher m2 = NUMBER_DV_CAPTURED_DV_CODE_SHIFTED_FROM_DV_CODE_REGEX_PATTERN.matcher(proposedName);
 			//6719_DV-Captured_P204_Shifted-From-M970_Residents-Meet_IIIS_8-Apr-2009_Tape2_Cam1
-			if(proposedName.matches("^" + NUMBER_REGEX + DV_CAPTURED_REGEX  + DV_CODE_REGEX + SHIFTED_FROM_REGEX + DV_CODE_REGEX)) {
-				idx = ordinalIndexOf(proposedName, "_", 4);
-				prefix = proposedName.substring(0, idx);   //6719_DV-Captured_P204_Shifted-From-M970
-				String prevCodes = prefix.replace(DV_CAPTURED_REGEX, "_").replace(SHIFTED_FROM_REGEX, "_");   //6719_P204_M970
+			if(m2.find()) {
+				String prevCodes = m2.group(1) + "_" + m2.group(2) + "_" + m2.group(3);   //6719_P204_M970
+				//String prevCodes = m1.group(3) + "_" + m1.group(2) + "_" + m1.group(1);   //M970_P204_6719
 				artifactAttributes.setPreviousCode(prevCodes);
 			}
 			artifactAttributes.setSequenceNumber(oldSeq);
@@ -77,8 +82,8 @@ public class VideoDigi2010 implements Artifactclass{
 		return artifactAttributes;
 	}
 	
-	private String getPreviousCodeFromShiftedToName(String shiftedToArtifactName) {
-		Matcher m1 = SHIFTED_DV_REGEX_PATTERN.matcher(shiftedToArtifactName);
+	private static String getPreviousCodeFromShiftedToName(String shiftedToArtifactName) {
+		Matcher m1 = SHIFTED_TO_DV_REGEX_PATTERN.matcher(shiftedToArtifactName);
 		if(m1.find()) {
 			return m1.group(2) + "_" + m1.group(1);
 		}
@@ -108,7 +113,7 @@ public class VideoDigi2010 implements Artifactclass{
 			}
 	    }
 		
-	    if((SHIFTED_DV_REGEX_PATTERN.matcher(artifactNameAsInCatalog).find() && artifactSize < 1024) // matches() or find() - regex seems to be made for find()
+	    if((SHIFTED_TO_DV_REGEX_PATTERN.matcher(artifactNameAsInCatalog).find() && artifactSize < 1024) // matches() or find() - regex seems to be made for find()
 	    		|| artifactNameAsInCatalog.equals("Z150_Shifted-to-Br-Category_Sw-Nirvichara-has-catalog-numbers_19-Dec-2010")
 	    		|| artifactNameAsInCatalog.equals("Z151_Shifted-to-Br-Category_Sw-Nirvichara-has-catalog-numbers_19-Dec-2010")
 	    		|| artifactNameAsInCatalog.equals("Z1179_Shifted-to-Brahmachari-Material_No-new-category-code-given-by-Swami-Nirvichara_15-Jan-2011"))
@@ -118,5 +123,48 @@ public class VideoDigi2010 implements Artifactclass{
 //	    	throw new Exception ("Dummy Shifted-to folder");
 		return true;
 	}
-}		
+	
+	public static void main(String[] args) {
 
+		String proposedName = "M904";
+		
+		SHIFTED_TO_MAP.put("M903","A1164_Shifted-to-M903_14-Oct-2010");
+		proposedName = "M903";
+		
+		proposedName = "5902_DV-Captured_A1929_Inner-Engineering_Tampa_Day3_Cam2_Tape2_10-Nov-06";
+		proposedName = "6719_DV-Captured_P204_Shifted-From-M970_Residents-Meet_IIIS_8-Apr-2009_Tape2_Cam1";
+		
+//		String shiftedToArtifactName = "A1164_Shifted-to-M903_14-Oct-2010";
+//		Matcher m1 = SHIFTED_TO_DV_REGEX_PATTERN.matcher(shiftedToArtifactName);
+//		if(m1.find()) {
+//			System.out.println(m1.group(2) + "_" + m1.group(1));
+//		}
+//
+//
+//		if(NUMBER_DV_CAPTURED_DV_CODE_REGEX_PATTERN.matcher(proposedName).matches()) {  //5902_DV-Captured_A1929_Inner-Engineering_Tampa_Day3_Cam2_Tape2_10-Nov-06
+//			System.out.println("im here");
+//		}
+		
+
+		Matcher m1 = NUMBER_DV_CAPTURED_DV_CODE_REGEX_PATTERN.matcher(proposedName);
+		if(proposedName.matches("^" + DV_CODE_REGEX + "$")) {
+			if(SHIFTED_TO_MAP.containsKey(proposedName))
+				System.out.println("SHIFTED_TO_MAP - " + getPreviousCodeFromShiftedToName(SHIFTED_TO_MAP.get(proposedName)));
+			else 
+				System.out.println("General - " + proposedName);
+			
+		} else if(m1.find()) {  //5902_DV-Captured_A1929_Inner-Engineering_Tampa_Day3_Cam2_Tape2_10-Nov-06
+			int oldSeq = Integer.parseInt(m1.group(1)); //5902
+			System.out.println("DVC - " + m1.group(1) + "_" + m1.group(2));  //5902_A1929
+			//artifactAttributes.setPreviousCode(m1.group(2) + "_" + m1.group(1));  //A1929_5902
+			
+			Matcher m2 = NUMBER_DV_CAPTURED_DV_CODE_SHIFTED_FROM_DV_CODE_REGEX_PATTERN.matcher(proposedName); //6719_DV-Captured_P204_Shifted-From-M970_Residents-Meet_IIIS_8-Apr-2009_Tape2_Cam1
+			if(m2.find()) {
+				String prevCodes = m2.group(1) + "_" + m2.group(2) + "_" + m2.group(3);   //6719_P204_M970
+				//String prevCodes = m1.group(3) + "_" + m1.group(2) + "_" + m1.group(1);   //M970_P204_6719
+				System.out.println("DVC SF - " + prevCodes);
+			}
+			System.out.println("DVC Seq num - " +oldSeq);
+		}
+	}		
+}
