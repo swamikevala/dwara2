4a8537fa556874b9af23813d2e0d81c809af9214
Prakash Gurumurthy <prakash.gurumurthy@ishafoundation.org>
2022-01-17 15:09:14 +0530

    Import Support Audio

---
+++ dwara-commons/src/main/java/org/ishafoundation/dwaraapi/artifact/artifactclass/impl/AudioPriv1.java
@@ -0,0 +1,9 @@
+package org.ishafoundation.dwaraapi.artifact.artifactclass.impl;
+
+import org.ishafoundation.dwaraapi.artifact.artifactclass.impl.super_.DefaultArtifactclassImpl;
+import org.springframework.stereotype.Component;
+
+@Component("audio-priv1")
+public class AudioPriv1 extends DefaultArtifactclassImpl{
+
+}
---
+++ dwara-commons/src/main/java/org/ishafoundation/dwaraapi/artifact/artifactclass/impl/AudioPriv2.java
@@ -0,0 +1,9 @@
+package org.ishafoundation.dwaraapi.artifact.artifactclass.impl;
+
+import org.ishafoundation.dwaraapi.artifact.artifactclass.impl.super_.DefaultArtifactclassImpl;
+import org.springframework.stereotype.Component;
+
+@Component("audio-priv2")
+public class AudioPriv2 extends DefaultArtifactclassImpl{
+
+}
