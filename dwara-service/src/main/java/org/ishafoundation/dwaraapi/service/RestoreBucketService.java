a01c252860abc54f88c34b1d2f86c8db3752e280
aumrit <aumrit.sarangi@ishafoundation.org>
2022-01-26 14:45:03 +0530

    added post call for subject

--- dwara-service/src/main/java/org/ishafoundation/dwaraapi/service/RestoreBucketService.java
+++ dwara-service/src/main/java/org/ishafoundation/dwaraapi/service/RestoreBucketService.java
@@ -197,7 +197,7 @@ public class RestoreBucketService extends DwaraService {
 		String emailBody = "<p>Namaskaram</p>";
 		User requester= userDao.findById(tRestoreBucket.getRequestedBy()).get();
         String requesterName= requester.getName();
-        emailBody += "<p>A private request has been raised by"+requesterName+"</p>";
+        emailBody += "<p>A private request has been raised by "+requesterName+"</p>";
         emailBody += "<p>The following folders in <span style='color:red'>red</span> need your approval.</p>";
 		List<String> fileName = new ArrayList<>();
 		for (RestoreBucketFile file : tRestoreBucket.getDetails()) {
@@ -214,7 +214,6 @@ public class RestoreBucketService extends DwaraService {
 		emailerService.sendEmail(emailBody);*/
 		String sendUrlTemplate= UriComponentsBuilder.fromHttpUrl(sendUrl)
 				.queryParam("concernedEmail" , tRestoreBucket.getApproverEmail() )
-				.queryParam("subject","Need Approval for project: _"+tRestoreBucket.getId()+"_. Priority: "+ tRestoreBucket.getPriority())
 
 				.queryParam("requesterEmail" , requester.getEmail())
 				.encode()
@@ -222,8 +221,10 @@ public class RestoreBucketService extends DwaraService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("X-COM-PERSIST", "true");
 		headers.set("X-COM-LOCATION", "USA");
-
-		HttpEntity<String> request = new HttpEntity<>(emailBody, headers);
+		List<String> emailArguments= new ArrayList<>();
+		emailArguments.add("Need Approval for project: _"+tRestoreBucket.getId()+"_. Priority: "+ tRestoreBucket.getPriority());
+		emailArguments.add(emailBody);
+		HttpEntity<List<String>> request = new HttpEntity<>(emailArguments, headers);
 		ResponseEntity<String> response1
 				= restTemplate.postForEntity( sendUrlTemplate,request, String.class);
 	}
