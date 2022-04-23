5d2e9aaf6ed0f3ba5d14647a74087698e64e5803
Prakash Gurumurthy <prakash.gurumurthy@ishafoundation.org>
2022-04-23 13:57:18 +0530

    Artifact event date to be shown during restore search

--- dwara-service/src/main/java/org/ishafoundation/dwaraapi/api/resp/dwarahover/DwaraHoverFileList.java
+++ dwara-service/src/main/java/org/ishafoundation/dwaraapi/api/resp/dwarahover/DwaraHoverFileList.java
@@ -1,14 +1,17 @@
 package org.ishafoundation.dwaraapi.api.resp.dwarahover;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Getter
 @Setter
@@ -19,13 +22,16 @@ public class DwaraHoverFileList {
 	private int id;
 	private String path_name;
 	private String short_path_name;
+	private String date;
 	private Long size;
 	private String size_in;
 	private String artifact_class_id;
 	private List<String> proxy_path_name;
 	private List<DwaraHoverTranscriptListDTO> transcripts;
 
-
+//	private static SimpleDateFormat artifactEventDateFormat = new SimpleDateFormat("dd-mmm-yyyy");
+	private static Pattern artifactEventDatePattern = Pattern.compile("_(([0-9]{2})-([A-Z][a-z]{2})-([0-9]{4}))");
+	
 	public static Optional<List<DwaraHoverFileList>> build(List<DwaraHoverFileListDTO> gotResponse) {
 		if (gotResponse == null) {
 			return Optional.empty();
@@ -34,14 +40,20 @@ public class DwaraHoverFileList {
 		List<DwaraHoverFileList> dwaraHoverFileLists = new ArrayList<>();
 
 		gotResponse.forEach(hoverList -> {
+			String filePathname = hoverList.getPathName();
+			String artifactName = filePathname.contains("/") ? StringUtils.substringBefore(filePathname, "/") : filePathname;
+//			Matcher m = artifactEventDateFormat.matcher(fileName);
+			Matcher m = artifactEventDatePattern.matcher(artifactName);
+			String artifactEventDate = m.find() ? m.group(1) : "";		
 			DwaraHoverFileList response = DwaraHoverFileList.builder()
 					.id(hoverList.getId() == 0 ? 0 : hoverList.getId())
-					.path_name(hoverList.getPathName())
-					.short_path_name(hoverList.getPathName().contains("/") ? StringUtils.substringBefore(hoverList.getPathName(), "/") + "..." + hoverList.getPathName().substring(hoverList.getPathName().length() - 10) : StringUtils.substring(hoverList.getPathName(), 0, 40) + "..." + hoverList.getPathName().substring(hoverList.getPathName().length() - 10))
+					.path_name(filePathname)
+					.short_path_name(filePathname.contains("/") ? StringUtils.substringBefore(filePathname, "/") + "..." + filePathname.substring(filePathname.length() - 10) : StringUtils.substring(filePathname, 0, 40) + "..." + filePathname.substring(filePathname.length() - 10))
 					.size(hoverList.getSize())
 					.size_in("B")
 					.artifact_class_id(!StringUtils.isEmpty(hoverList.getArtifactClass_id()) ? hoverList.getArtifactClass_id() : null)
 					.proxy_path_name(hoverList.getProxyPathName())
+					.date(artifactEventDate)
 					.transcripts(hoverList.getTranscripts())
 					.build();
 
