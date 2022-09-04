cef07095b870107e6c702804f3268948b2e27bc0
Prakash Gurumurthy <prakash.gurumurthy@ishafoundation.org>
2022-09-04 18:16:45 +0000

    dwara_updata_2.1.32.sql edited online with Bitbucket

--- dwara-db/src/data/sql/dwara_updata_2.1.32.sql
+++ dwara-db/src/data/sql/dwara_updata_2.1.32.sql
@@ -1,13 +1,16 @@
 -- ********* Adding config for Edited Audio *********
 -- ARTIFACTCLASS --
-INSERT INTO `artifactclass` (`id`, `description`, `sequence_id`, `source`, `concurrent_volume_copies`, `display_order`, `path_prefix`, `artifactclass_ref_id`, `import_only`) VALUES 
-('photo-priv1', 'photo priv 1', 'photo-pub', 1, 1, 1, '/data/dwara/staged', null, 0);
+INSERT INTO `artifactclass` (`id`, `description`, `sequence_id`, `source`, `concurrent_volume_copies`, `display_order`, `path_prefix`, `artifactclass_ref_id`, `import_only`, `config`) VALUES 
+('photo-priv1', 'photo priv 1', 'photo-pub', 1, 1, 1, '/data/dwara/staged', null, 0, null),
+('photo-priv1-proxy', 'photo priv 1 proxy', 'photo-pub-proxy', 0, 1, 0, '/data/dwara/transcoded', 'photo-priv1', 0, '{"pathname_regex": "(?!)"}');
  
 -- ARTIFACTCLASS_VOLUME --
 INSERT INTO `artifactclass_volume` (`artifactclass_id`, `volume_id`, `encrypted`, `active`) VALUES 
 ('photo-priv1', 'R1', 0, 1),
 ('photo-priv1', 'R2', 0, 1),
-('photo-priv1', 'R3', 0, 1);
+('photo-priv1', 'R3', 0, 1),
+('photo-priv1-proxy', 'G1', 0, 1),
+('photo-priv1-proxy', 'G2', 0, 1);
 
 -- ACTION_ARTIFACTCLASS_USER --
 INSERT INTO `action_artifactclass_user` (`action_id`, `artifactclass_id`, `user_id`) VALUES 
